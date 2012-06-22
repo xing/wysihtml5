0fdff1dd589563de1b226bcc854010c7e751267a
Xavier Spriet <linkadmin@gmail.com>
2012-06-22 17:16:37 -0400

    Fix DOM classList handling
    * When the DOMTokenList is empty on Chrome, it returns undefined
    * This patch ensures the element classList can be resolved before using it

--- src/dom/class.js
+++ src/dom/class.js
@@ -1,30 +1,30 @@
 (function(wysihtml5) {
   var supportsClassList = wysihtml5.browser.supportsClassList(),
       api               = wysihtml5.dom;
-  
+
   api.addClass = function(element, className) {
-    if (supportsClassList) {
-      return element.classList.add(className);
+    if (supportsClassList && ((classList = element.classList) != null)) {
+      return classList.add(className);
     }
     if (api.hasClass(element, className)) {
       return;
     }
     element.className += " " + className;
   };
-  
+
   api.removeClass = function(element, className) {
-    if (supportsClassList) {
-      return element.classList.remove(className);
+    if (supportsClassList && ((classList = element.classList) != null)) {
+      return classList.remove(className);
     }
-    
+
     element.className = element.className.replace(new RegExp("(^|\\s+)" + className + "(\\s+|$)"), " ");
   };
-  
+
   api.hasClass = function(element, className) {
-    if (supportsClassList) {
-      return element.classList.contains(className);
+    if (supportsClassList && ((classList = element.classList) != null)) {
+      return classList.contains(className);
     }
-    
+
     var elementClassName = element.className;
     return (elementClassName.length > 0 && (elementClassName == className || new RegExp("(^|\\s)" + className + "(\\s|$)").test(elementClassName)));
   };
