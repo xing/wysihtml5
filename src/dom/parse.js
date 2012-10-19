9027cc0bdc73a3ef3ea66100bd528780aad7a630
Jonathan Barrett <jonathan@freeagentcentral.com>
2012-10-19 10:06:40 +0100

    [#187] adds style rule to check_attributes handling

--- src/dom/parse.js
+++ src/dom/parse.js
@@ -411,6 +411,13 @@ wysihtml5.dom.parse = (function() {
         attributeValue = (attributeValue || "").replace(REG_EXP, "");
         return attributeValue || null;
       };
+    })(),
+
+    style: (function() {
+      var REG_EXP = /[^a-z0-9_\-]\:[^a-z0-9_\-];/gi;
+      return function(attributeValue) {
+        return attributeValue;
+      };
     })()
   };
   
