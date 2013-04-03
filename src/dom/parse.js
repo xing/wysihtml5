5db88c0e5343769eefec7c5f9461d4f029ce8ebe
shaunfreeman <shaun@shaunfreeman.co.uk>
2013-04-03 14:15:47 +0100

    added an option to allow all classes when paring, this is a fix for #219

--- src/dom/parse.js
+++ src/dom/parse.js
@@ -214,6 +214,7 @@ wysihtml5.dom.parse = (function() {
         setAttributes       = rule.set_attributes,        // attributes to set on the current node
         checkAttributes     = rule.check_attributes,      // check/convert values of attributes
         allowedClasses      = currentRules.classes,
+        allowEveryClasses 	= currentRules.allowAllClasses,
         i                   = 0,
         classes             = [],
         newClasses          = [],
@@ -272,7 +273,7 @@ wysihtml5.dom.parse = (function() {
     classesLength = classes.length;
     for (; i<classesLength; i++) {
       currentClass = classes[i];
-      if (allowedClasses[currentClass]) {
+      if (allowedClasses[currentClass] || allowEveryClasses) {
         newClasses.push(currentClass);
       }
     }
